06ebbc0bc252a9bc46a8d44ac322acf14545c0fe
MacuKodear <mperez@kodear.net>
2021-08-26 09:03:53 -0300

    se agrego component calculator

--- src/app/calculator/calculator.module.ts
+++ src/app/calculator/calculator.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ButtonsComponent } from './components/buttons/buttons.component';
 import { ScreenComponent } from './components/screen/screen.component';
 import { FiltersThemeComponent } from './components/filters-theme/filters-theme.component';
+import { CalculatorComponent } from '../pages/calculator/calculator.component';
+import { CalculatorRoutingModule } from './calculator-routing.module';
 
 
 
@@ -10,10 +12,13 @@ import { FiltersThemeComponent } from './components/filters-theme/filters-theme.
   declarations: [
     ButtonsComponent,
     ScreenComponent,
-    FiltersThemeComponent
+    FiltersThemeComponent,
+    CalculatorComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CalculatorRoutingModule
+
   ]
 })
 export class CalculatorModule { }
